21f9ecec05dcae4e42dd0653d8d7931c348141a6
Max Jourdain <maximilien.jourdain@digitaslbi.fr>
2017-10-30 18:28:43 +0100

    Testing mobile or not

--- js/mobile-image-loader.js
+++ js/mobile-image-loader.js
@@ -1,6 +1,7 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && path) {
   let coverEl = document.getElementsByClassName('image-cropper')[0];
   coverEl.style.backgroundImage = "url("+preview+")";
-  let avatar = document.getElementsByClassName('site-avatar')[0];
-  avatar.style.display = "none"; 
+  alert("mobile");
+} else {
+  alert("not mobile");
 }
