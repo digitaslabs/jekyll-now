3be27213558c890748710ebfaa33cd3d64728804
Max Jourdain <maximilien.jourdain@digitaslbi.fr>
2017-10-30 18:05:38 +0100

    Testing

--- js/mobile-image-loader.js
+++ js/mobile-image-loader.js
@@ -1,4 +1,6 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && path) {
   let coverEl = document.getElementsByClassName('image-cropper')[0];
   coverEl.style.backgroundImage = "url("+preview+")";
+  let avatar = document.getElementsByClassName('site-avatar')[0];
+  avatar.style.display = "none"; 
 }
